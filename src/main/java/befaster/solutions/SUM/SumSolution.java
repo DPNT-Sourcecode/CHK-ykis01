a17f681cfd9f8ce8e316ab0a343642ade379f0f4
sbx_user1051 <sbx_user1051@169.254.6.73>
2024-02-25 17:04:09 +0000

    Sun Feb 25 17:04:09 UTC 2024

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -6,7 +6,24 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+        return sum(x, y);
+    }
+    
+    // sum(Integer, Integer) -> Integer
+    
+//    - param[0] = a positive integer between 0-100
+//    - param[1] = a positive integer between 0-100
+//    - @return = an Integer representing the sum of the two numbers
+
+    private static int sum(int x, int y) {
+        if (x < 0 || x > 100) {
+            throw new IllegalArgumentException("x must be a positive integer between 0-100");
+        }
+        if (y < 0 || y > 100) {
+            throw new IllegalArgumentException("y must be a positive integer between 0-100");
+        }
+        return x + y;
     }
 
 }
+
