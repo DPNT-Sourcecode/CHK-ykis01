716ab24475eda3afee04c5f526118f74dfc7be11
sbx_user1051 <sbx_user1051@169.254.6.73>
2024-02-25 19:43:30 +0000

    Sun Feb 25 19:43:30 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -16,6 +16,7 @@ public class CheckoutSolution {
         inventory.put('C', new PricingInfo(20));
         inventory.put('D', new PricingInfo(15));
         inventory.put('E', new PricingInfo(40, Collections.emptyMap(), Map.of(2, new FreeProductDiscount(1, 'B'))));
+        inventory.put('F', new PricingInfo(40, Collections.emptyMap(), Map.of(2, new FreeProductDiscount(1, 'F'))));
     }
 
     public Integer checkout(String skus) {
@@ -47,8 +48,9 @@ public class CheckoutSolution {
 
             // Iterate each of the bundle offers and apply them, from the best to the worst
             for (Entry<Integer, FreeProductDiscount> offer : productPricing.getSpecialBundleOffers().entrySet()) {
+                // Calculates the total of units that can be discounted through this offer
                 int totalDiscountedUnits = (quantity / offer.getKey()) * offer.getValue().getDiscountedUnits();
-                // Removes one from the quantity
+                // Removes those units from the quantity
                 products.computeIfPresent(offer.getValue().getDiscountedSku(), (k, v) -> v - totalDiscountedUnits);
                 // save the rest to be evaluated next
                 quantity = quantity % offer.getKey();
@@ -138,5 +140,6 @@ public class CheckoutSolution {
         }
     }
 }
+
 
 
