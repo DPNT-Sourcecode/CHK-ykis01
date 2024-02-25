f0048ffcb1ec02276a8a01066117f25b1aff7674
sbx_user1051 <sbx_user1051@169.254.6.73>
2024-02-25 19:04:31 +0000

    Sun Feb 25 19:04:31 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,10 +3,11 @@ package befaster.solutions.CHK;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CheckoutSolution {
     private Map<Character, PricingInfo> inventory;
-    
+
     public CheckoutSolution() {
         this.inventory = new HashMap<>();
         inventory.put('A', new PricingInfo(50, Map.of(3, 130, 5, 200)));
@@ -14,64 +15,61 @@ public class CheckoutSolution {
         inventory.put('C', new PricingInfo(20));
         inventory.put('D', new PricingInfo(15));
     }
-    
+
     public Integer checkout(String skus) {
         // Validate if the skus aren't null
         if (skus == null) {
             return -1;
         }
-        
+
         // Validate if there aren't any illegal products
         for (char sku : skus.toCharArray()) {
             if (!inventory.containsKey(sku)) {
                 return -1;
             }
         }
-        
+
         // Build the sum of all the products
         Map<Character, Integer> products = new HashMap<>();
         for (char sku : skus.toCharArray()) {
             products.compute(sku, (k, v) -> v == null ? 1 : v + 1);
         }
-        
+
         // Sum products based on their quantity
         int total = 0;
         for (Map.Entry<Character, Integer> productQuantity : products.entrySet()) {
             Character sku = productQuantity.getKey();
             Integer quantity = productQuantity.getValue();
-            
+
             // Get product pricing
             PricingInfo productPricing = inventory.get(sku);
-            
+
             // If it doesn't have any special offers, then use the regular pricing
             if (productPricing.getSpecialQuantityOffers() == null) {
                 total += quantity * productPricing.getRegularPrice();
                 continue;
             }
-            
+
             // If it has special pricing, then handle it accordingly
-            // First calculate the special price for the available "bundles"
-            int specialPriceItems = (quantity / productPricing.getSpecialQuantityOffers().getKey()) * productPricing.getSpecialQuantityOffers().getValue();
-            // Then add the extras as regular priced
-            int regularPriceItems = (quantity % productPricing.getSpecialQuantityOffers().getKey()) * productPricing.getRegularPrice();
-            total += specialPriceItems + regularPriceItems;
+
         }
-        
+
         return total;
     }
-    
+
     class PricingInfo {
         private final int regularPrice;
         private final Map<Integer, Integer> specialQuantityOffer;
-        
+
         PricingInfo(int regularPrice) {
             this.regularPrice = regularPrice;
             this.specialQuantityOffer = Collections.emptyMap();
         }
-        
+
         PricingInfo(int regularPrice, Map<Integer, Integer> specialQuantityOffers) {
             this.regularPrice = regularPrice;
-            this.specialQuantityOffer = specialQuantityOffers;
+            this.specialQuantityOffer = new TreeMap<>(Collections.reverseOrder());
+            this.specialQuantityOffer.putAll(specialQuantityOffers);
         }
 
         public int getRegularPrice() {
@@ -88,4 +86,5 @@ public class CheckoutSolution {
         }
     }
 }
+
 
