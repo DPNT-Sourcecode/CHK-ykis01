00575e6ba391ec1e87281f31ef9fcf4896510acd
sbx_user1051 <sbx_user1051@169.254.6.73>
2024-02-25 17:06:46 +0000

    Sun Feb 25 17:06:46 UTC 2024

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -1,11 +1,10 @@
 package befaster.solutions.SUM;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SumSolutionTest {
     private SumSolution sum;
@@ -16,7 +15,29 @@ public class SumSolutionTest {
     }
 
     @Test
-    public void compute_sum() {
+    public void testSum_whenXAndYAreInBounds_returnsSum() {
         assertThat(sum.compute(1, 1), equalTo(2));
     }
+
+    @Test
+    public void testSum_whenXIsOutOfLowerBounds_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sum.compute(-1, 1));
+    }
+
+    @Test
+    public void testSum_whenXIsOutOfUpperBounds_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sum.compute(101, 1));
+    }
+
+    @Test
+    public void testSum_whenYIsOutOfLowerBounds_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sum.compute(1, -1));
+    }
+
+    @Test
+    public void testSum_whenYIsOutOfUpperBounds_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sum.compute(1, 101));
+    }
+
 }
+
