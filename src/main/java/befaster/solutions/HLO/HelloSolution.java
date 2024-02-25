8cea101d1bf2361a27020e72a3b70563d8ac442e
sbx_user1051 <sbx_user1051@169.254.6.73>
2024-02-25 17:09:40 +0000

    Sun Feb 25 17:09:40 UTC 2024

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -1,9 +1,16 @@
 package befaster.solutions.HLO;
 
+import java.util.Objects;
 import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
         throw new SolutionNotImplementedException();
     }
+    
+    private String sayHello(String name) {
+        Objects.requireNonNull(name, "The value null is not a valid name");
+        return "Hello, " + name + "!";
+    }
 }
+
